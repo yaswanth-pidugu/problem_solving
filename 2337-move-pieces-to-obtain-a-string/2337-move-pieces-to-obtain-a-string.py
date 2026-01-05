ef4acdfbf28e598722e3c14447d67b037d1494b1
Yaswanth Pidugu <yashwanthpidugu999@gmail.com>
2026-01-05 08:20:15 +0530

    Time: 210 ms (18.71%), Space: 18.6 MB (37.13%) - LeetHub

---
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canChange(self, start: str, target: str) -> bool:
+        ss = ""
+        for s in start:
+            if s == "L" or s == "R":
+                ss += s
+        tt = ""
+        for t in target:
+            if t == "L" or t == "R":
+                tt += t
+        if ss != tt:
+            return False
+        n = len(start)
+        i = 0
+        j = 0
+        while i < n or j < n:
+            while i < n and start[i] == "_":
+                i+=1
+            while j < n and target[j] == "_":
+                j+=1
+            if i == n and j == n:
+                return True
+            if i == n or j == n:
+                return False
+            if start[i] != target[j]:
+                return False
+            if start[i] == "L" and i < j:
+                return False
+            if start[i] == "R" and i > j:
+                return False
+            i+=1
+            j+=1
+        return True

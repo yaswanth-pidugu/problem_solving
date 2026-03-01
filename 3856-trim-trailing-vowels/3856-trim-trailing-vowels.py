af2045b696b57d95c6ad10559051c9e151c36a25
Yaswanth Pidugu <yashwanthpidugu999@gmail.com>
2026-03-01 10:12:06 +0530

    Time: 2 ms (18.67%), Space: 19.1 MB (94.18%) - LeetHub

--- 3856-trim-trailing-vowels/3856-trim-trailing-vowels.py
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.py
@@ -1,9 +1,3 @@
 class Solution:
     def trimTrailingVowels(self, s: str) -> str:
-        r = len(s) - 1
-        while r >= 0:
-            if s[r] in ('a', 'e', 'i', 'o', 'u'):
-                r-=1
-            else:
-                break
-        return s[:r+1]
+        return s.rstrip("aeiou")
